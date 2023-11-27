5cabb5fbff47b969d6084d9c9f7148e9c28c59c4
trschwab <trschwab7@gmail.com>
2023-11-27 17:53:14 -0500

    This just got very cool very quickly. Lots of improvement can be made here but leveraging rest api is a must going forwads

--- api_test/api_project/base/models.py
+++ api_test/api_project/base/models.py
@@ -3,7 +3,11 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=200)
     day = models.CharField(max_length=200)
+    month = models.CharField(max_length=200)
+    year = models.CharField(max_length=200)
     film = models.CharField(max_length=200)
     released = models.CharField(max_length=200)
     rating = models.CharField(max_length=200)
+    review_link = models.CharField(max_length=200)
+    film_link = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
