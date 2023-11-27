ea45faacf5fcb709eaa7caee4517d38fd7067466
trschwab <trschwab7@gmail.com>
2023-11-27 13:31:27 -0500

    alter fields, post and get from notebook

--- api_test/api_project/base/models.py
+++ api_test/api_project/base/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=200)
+    day = models.CharField(max_length=200)
+    film = models.CharField(max_length=200)
+    released = models.CharField(max_length=200)
+    rating = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
